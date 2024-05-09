3cab08e7ea549fcd5a7fded543cdb17e857435d3
emre1n <emre1n@protonmail.com>
2024-05-09 18:04:34 +0300

    fix login action

--- src/actions/db/login.ts
+++ src/actions/db/login.ts
@@ -22,7 +22,7 @@ export async function login(data: LoginFormValuesType) {
     await signIn('credentials', {
       email,
       password,
-      redirectTo: DEFAULT_LOGIN_REDIRECT,
+      // redirectTo: DEFAULT_LOGIN_REDIRECT,
     });
   } catch (error) {
     if (error instanceof AuthError) {
