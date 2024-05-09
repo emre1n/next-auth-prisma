e74b9fc83c9d6c4a6e1d9a972eda1f53ff65cafb
emre1n <emre1n@protonmail.com>
2024-05-09 18:17:24 +0300

    check session

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,13 @@
+import { auth } from '@/auth';
 import LoginForm from '@/components/LoginForm';
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+
   return (
     <div className="w-full flex flex-col gap-8">
       <h1 className="text-2xl">Login</h1>
+      <p>{session ? JSON.stringify(session?.user?.email) : 'no session'}</p>
       <LoginForm />
     </div>
   );
