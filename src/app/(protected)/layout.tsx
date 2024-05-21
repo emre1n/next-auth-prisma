210326b9814cc77b1543a3a791aad8633acad3c6
emre1n <emre1n@protonmail.com>
2024-05-21 19:21:43 +0300

    fix height of settings page

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -4,7 +4,7 @@ interface ProtectedLayoutProps {
 
 export default function ProtectedLayout({ children }: ProtectedLayoutProps) {
   return (
-    <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center bg-slate-200">
+    <div className="main-height w-full flex flex-col gap-y-10 items-center justify-center bg-slate-200">
       {children}
     </div>
   );
